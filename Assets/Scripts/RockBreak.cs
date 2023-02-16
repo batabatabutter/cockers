6aec6d4d4709461aa39f6524019153a5dfea6e26
TSITCL\it222176 <annoguijiaremu@gmail.com>
2023-02-16 13:05:33 +0900

    岩のギミック追加

---
+++ Assets/Scripts/RockBreak.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBreak : MonoBehaviour
+{
+    bool Rock = true;
+    GameObject Object_Rock;
+    [SerializeField] GameObject text;
+    [SerializeField] GameObject text2;
+    float time = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object_Rock = GameObject.Find("Rock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("‰ó‚ê‚½");
+            text.SetActive(true);
+            Destroy();
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("‰ó‚ê‚È‚¢");
+            text2.SetActive(true);
+            DontDestroy();
+        }
+
+        //2•bŒã‚ÉÁ‚¦‚é
+        if (text.gameObject.activeSelf == true) time += Time.deltaTime;
+        if (time >= 2.0f)
+        {
+            text.SetActive(false);
+            time = 0f;
+        }
+        if (text2.gameObject.activeSelf == true) time += Time.deltaTime;
+        if (time >= 2.0f)
+        {
+            text2.SetActive(false);
+            time = 0f;
+        }
+
+    }
+
+    //Šâ‚Ì”j‰ó
+    public void Destroy()
+    {
+        Object_Rock.SetActive(false);
+    }
+
+    //Šâ‚ª”j‰óo—ˆ‚È‚¢
+    public void DontDestroy()
+    {
+        Object_Rock.SetActive(true);
+    }
+}
