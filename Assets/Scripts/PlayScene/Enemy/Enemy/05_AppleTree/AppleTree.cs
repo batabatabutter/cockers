6ac8c05766924cbe12fd50be054e8776f2f823b5
{nerner} <it222253@gmail.com>
2023-03-14 15:44:13 +0900

    敵追加②

---
+++ Assets/Scripts/PlayScene/Enemy/Enemy/05_AppleTree/AppleTree.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : Enemy
+{
+
+    public override void EnemyStart()
+    {
+
+    }
+
+    //  çXêV
+    public override void EnemyUpdate()
+    {
+    }
+}
