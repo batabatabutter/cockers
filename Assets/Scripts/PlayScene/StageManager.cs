7b84d0e87172b3fc15ebfcebf7c7e8d8ad692503
batabatabutter <116615347+batabatabutter@users.noreply.github.com>
2023-03-17 22:26:57 +0900

    フィールド生成の管理追加

--- Assets/Scripts/PlayScene/StageManager.cs
+++ Assets/Scripts/PlayScene/StageManager.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public enum StageID
 {
-    [InspectorName("ステージ1")] Stage1,
-    [InspectorName("ステージ2")] Stage2,
-    [InspectorName("ステージ3")] Stage3,
-    [InspectorName("ステージ4")] Stage4,
-    [InspectorName("ステージ5")] Stage5,
-    [InspectorName("ステージ6")] Stage6,
+    [InspectorName("ステージ1初期")] Stage1,
+    [InspectorName("ステージ2初期")] Stage2,
+    [InspectorName("ステージ3初期")] Stage3,
+    [InspectorName("ステージ4初期")] Stage4,
+    [InspectorName("ステージ5初期")] Stage5,
+    [InspectorName("ステージ6初期")] Stage6,
 
     [InspectorName("")] StageNum
+}
+
+[System.Serializable]
+public class StageData
+{
+    [SerializeField, Label("ロビー")] GameObject loby;
+    [SerializeField, Label("フィールド")] List<GameObject> Fields;
+
+    public GameObject GetLoby() { return loby; }         //  ロビー受渡
+    public GameObject GetField(int fieldID) { if (Fields.Count <= fieldID) return null; return Fields[fieldID]; }    //  フィールド受渡
 }
 
 public class StageManager : MonoBehaviour
@@ -19,15 +29,32 @@ public class StageManager : MonoBehaviour
     [SerializeField, Label("デバッグ用ステージ番号")] StageID stageID;
     [SerializeField, Label("ステージ")] List<GameObject> stageObjects;
 
-    private void Start()
-    {
-        CreateStage(stageID);
-    }
+    [SerializeField, Label("ステージ")] List<StageData> stages;
+
+    private GameObject nowField;
 
     //  ステージ生成
-    private void CreateStage(StageID stageID)
+    public void CreateStage(StageID stageID)
     {
+        if (nowField != null) Destroy(nowField);
         if (stageObjects[(int)stageID] == null) Debug.Log("Error:" + stageID + "対応のプレハブがありません");
-        GameObject stage = Instantiate(stageObjects[(int)stageID], Vector3.zero, Quaternion.identity);
+        nowField = Instantiate(stageObjects[(int)stageID], Vector3.zero, Quaternion.identity);
+    }
+
+    //  フィールド生成
+    public void CreateField(StageID stageID, int fieldID)
+    {
+        if (nowField != null) Destroy(nowField);
+        if (stages[(int)stageID].GetField(fieldID) == null)
+        {
+            Debug.Log("Error:" + stageID + fieldID + "対応のプレハブがありません");
+        }
+        nowField = Instantiate(stages[(int)stageID].GetField(fieldID), Vector3.zero, Quaternion.identity);
+    }
+
+    //  デバッグ用生成
+    public void DebugCreateStage()
+    {
+        CreateStage(stageID);
     }
 }
