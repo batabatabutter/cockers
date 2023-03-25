8c9c7c80a5ff7f96480d97e5e7cc985e8aae5236
HaiRu <annoguijiaremu@gmail.com>
2023-03-25 21:29:45 +0900

    テキスト表示

--- Assets/Scripts/Gimmick/Hunt.cs
+++ Assets/Scripts/Gimmick/Hunt.cs
@@ -1,44 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[System.Serializable]
-public class Ordeal
-{
-    [SerializeField, Label("種類")] EnemyID enemySpawnID;
-    public EnemyID GetEnemyID() { return enemySpawnID; }         //  出現種類受渡
-}
+using UnityEngine.InputSystem;
 
 
 public class Hunt : MonoBehaviour
 {
     bool EnterFlag = false;
-    [SerializeField, Label("敵の生成データ")] List<Ordeal> ordeals;
 
-    bool IsSpawn = false;
+    GameObject pObject;
+    GameObject eObject;
+    GameObject iObject;
 
-    public List<Ordeal> GetOrdeals()
-    {
-        return ordeals;
-    }
+    private PlayerStatus player = null;
+    private EnemyManager enemy = null;
+    private ItemManager item = null;
+
+    [SerializeField] GameObject text;
+
+    public List<Vector3> Enemy = new List<Vector3>();
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        pObject = GameObject.Find("PlayManager");
+        eObject = GameObject.Find("EnemyManager");
+        iObject = GameObject.Find("ItemManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        IsSpawn = EnterFlag;
+        var current = Keyboard.current;
 
+        if (enemy == null) enemy = eObject.GetComponent<EnemyManager>();
+
+        if (EnterFlag == true)
+        {
+            text.SetActive(true);
+            if (current.cKey.wasPressedThisFrame)
+            {
+                EnemySpawn();
+            }
+        }
+
+        if (EnterFlag == false) text.SetActive(false);
     }
+
+    public void EnemySpawn()
+    {
+        for (int i = 0; i < Enemy.Count; ++i)
+        {
+            enemy.GenerateEnemy(EnemyID.CarrotSpear, Enemy[i]);
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
             EnterFlag = true;
         }
@@ -46,19 +68,10 @@ public class Hunt : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "Player")
+        if (other.gameObject.CompareTag("Player"))
         {
             EnterFlag = false;
         }
     }
 
-    public bool GetIsSpawn()
-    {
-        return IsSpawn;
-    }
-
-    public void SetIsSpawn(bool isSpawn)
-    {
-        this.IsSpawn = isSpawn;
-    }
 }
